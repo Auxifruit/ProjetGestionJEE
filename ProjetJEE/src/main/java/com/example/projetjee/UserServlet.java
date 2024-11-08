44502a8a5a5a6b1a8f5cf21e6059173d0ace80f7
Guillaume B. <105287640+Auxifruit@users.noreply.github.com>
2024-11-08 16:18:07 +0100

    Pour la liste d'utilisateur

---
+++ ProjetJEE/src/main/java/com/example/projetjee/UserServlet.java
@@ -0,0 +1,36 @@
+package com.example.projetjee;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import models.Etudiant;
+import models.Utilisateur;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "userServlet", value = "/user-servlet")
+public class UserServlet extends HttpServlet {
+    private final UtilisateurBDD userBDD = new UtilisateurBDD();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String roleFilter = request.getParameter("roleFilter");
+        System.out.println(roleFilter);
+        List<Utilisateur> usersList = userBDD.getAllUsers(roleFilter);
+
+        // Set the list of students as a request attribute
+        request.setAttribute("users", usersList);
+
+        // Forward the request to result.jsp
+        try {
+            request.getRequestDispatcher("/roleList.jsp").forward(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void destroy() {
+    }
+}

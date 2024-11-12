f32ddaec8bb399a9938fb5e8c279e833dc22ea92
Auxifruit <auxi.fruit@gmail.com>
2024-11-12 21:32:16 +0100

    Ajout, modif, supp des matières

--- ProjetJEE/src/main/java/com/example/projetjee/model/dao/SubjectDAO.java
+++ ProjetJEE/src/main/java/com/example/projetjee/model/dao/SubjectDAO.java
@@ -35,6 +35,68 @@ public class SubjectDAO {
         return subjects;
     }
 
+    public static boolean addSubjectInTable(String subjectName) {
+        if(subjectName == null || subjectName.isEmpty()) {
+            return false;
+        }
+
+        try {
+            Connection connection = DatabaseManager.getConnection();
+
+            String query = "INSERT INTO " + SUBJECT_TABLE + "(" + SUBJECT_NAME + ") VALUES (?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+            preparedStatement.setString(1, subjectName);
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Une erreur est survenue lors de l'ajout d'une matière dans la base de donnée");
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean deleteSubjectFromTable(int subjectId) {
+        try {
+            Connection connection = DatabaseManager.getConnection();
+
+            String query = "DELETE FROM " + SUBJECT_TABLE + " WHERE " + SUBJECT_ID + " = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+            preparedStatement.setInt(1, subjectId);
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Une erreur est survenue lors de la suppression de la matière de la base de donnée");
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean modifySubjectFromTable(int subjectId, String subjectNewName) {
+        try {
+            Connection connection = DatabaseManager.getConnection();
+
+            String query = "UPDATE " + SUBJECT_TABLE + " SET " + SUBJECT_NAME + " = ? WHERE " + SUBJECT_ID + " = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+            preparedStatement.setString(1, subjectNewName);
+            preparedStatement.setInt(2, subjectId);
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Une erreur est survenue lors de la modification de la matière de la base de donnée");
+            return false;
+        }
+        return true;
+    }
+
     public static String getSubjectNameById(int subjectId) {
         String subjectName = " ";
 
@@ -55,5 +117,33 @@ public class SubjectDAO {
             e.printStackTrace();
         }
         return subjectName;
+    }
+
+    public static boolean isSubjectInTable(String subjectName) {
+        if(subjectName == null || subjectName.isEmpty()) {
+            return false;
+        }
+        boolean isIn = true;
+
+        try {
+            Connection connection = DatabaseManager.getConnection();
+
+            String query = "SELECT COUNT(*) FROM " + SUBJECT_TABLE + " WHERE " + SUBJECT_NAME + " LIKE(?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+            preparedStatement.setString(1, subjectName);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                int count = resultSet.getInt(1);
+                isIn = count != 0;
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return isIn;
     }
 }

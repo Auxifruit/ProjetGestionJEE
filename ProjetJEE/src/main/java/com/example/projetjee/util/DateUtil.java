6c714c229c372f3678666f2648ea45898db82d61
Auxifruit <auxi.fruit@gmail.com>
2024-11-14 18:06:46 +0100

    Ajout méthode pour avoir des timestamp

--- ProjetJEE/src/main/java/com/example/projetjee/util/DateUtil.java
+++ ProjetJEE/src/main/java/com/example/projetjee/util/DateUtil.java
@@ -1,7 +1,11 @@
 package com.example.projetjee.util;
 
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeParseException;
+import java.util.Date;
 
 public class DateUtil {
     public static boolean areDatesValid(String startDateString, String endDateString) {
@@ -13,6 +17,19 @@ public class DateUtil {
         } catch (DateTimeParseException e) {
             System.out.println("Erreur de format de date : " + e.getMessage());
             return false;
+        }
+    }
+
+    public static Timestamp dateStringToTimestamp(String dateString) {
+        try {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+            Date parsedDate = dateFormat.parse(dateString);
+            return new Timestamp(parsedDate.getTime());
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
         }
     }
 

85ca06534b8a82954c6d94ae9c975269b3e076a2
Hicham Bettahar <105277484+Poupouski@users.noreply.github.com>
2024-11-24 16:24:20 +0100

    Factorisation du code

--- ProjetJEE/src/main/java/com/example/projetjee/controller/LoginServlet.java
+++ ProjetJEE/src/main/java/com/example/projetjee/controller/LoginServlet.java
@@ -1,6 +1,7 @@
 package com.example.projetjee.controller;
 
 import com.example.projetjee.model.dao.UserDAO;
+import com.example.projetjee.util.HashPswdUtil;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -19,33 +20,6 @@ public class LoginServlet extends HttpServlet {
     private static final String DB_USERNAME = "root"; // Change en fonction de ton utilisateur MySQL
     private static final String DB_PASSWORD = ""; // Ton mot de passe MySQL
 
-    public static String hashPassword(String password) {
-        try {
-            // Créer une instance de MessageDigest pour SHA-256
-            MessageDigest digest = MessageDigest.getInstance("SHA-256");
-
-            // Convertir le mot de passe en tableau de bytes
-            byte[] encodedHash = digest.digest(password.getBytes());
-
-            // Convertir le tableau de bytes en chaîne hexadécimale
-            StringBuilder hexString = new StringBuilder();
-            for (byte b : encodedHash) {
-                String hex = Integer.toHexString(0xff & b);
-                if (hex.length() == 1) {
-                    hexString.append('0');
-                }
-                hexString.append(hex);
-            }
-
-            // Renvoyer une version tronquée à 50 caractères
-            return hexString.substring(0, 49);
-
-        } catch (NoSuchAlgorithmException e) {
-            // Gérer l'erreur si l'algorithme n'est pas disponible
-            throw new RuntimeException("Erreur : Algorithme SHA-256 non disponible", e);
-        }
-    }
-
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -58,7 +32,7 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String email = request.getParameter("email");
         String password = request.getParameter("password");
-        String hashedPassword = hashPassword(password);
+        String hashedPassword = HashPswdUtil.hashPassword(password);
 
         if(email == null || email.isEmpty()) {
             request.setAttribute("error", "Erreur : Veuillez saisir l'email.");

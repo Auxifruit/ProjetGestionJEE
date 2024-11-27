2c877b1bf4ff338a67939ac091b3ae4fbdbe2307
Guillaume B. <105287640+Auxifruit@users.noreply.github.com>
2024-11-27 16:55:09 +0100

    Add files via upload

---
+++ ProjetJEE/src/main/java/com/example/projetjee/controller/CreateDatabaseServlet.java
@@ -0,0 +1,23 @@
+package com.example.projetjee.controller;
+
+import com.example.projetjee.util.DatabaseUtil;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "CreateDatabaseServlet", value = "/createDatabase-servlet")
+public class CreateDatabaseServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        DatabaseUtil.createDatabase();
+    }
+}
